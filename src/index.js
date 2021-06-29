552f06a86c89d761c0c69fd2d22baaf3c1f94e53
SilvaJ3 <80819497+SilvaJ3@users.noreply.github.com>
2021-06-29 11:31:02 +0200

    initialisation du projet

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+// Import bootstrap
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import  "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+
+// Import fontawesome
+import  '@fortawesome/fontawesome-free/js/all';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +20,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
